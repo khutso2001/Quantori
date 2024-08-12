312b05875c3f3a9fadf1f727775cdef4fe4ccf7e
Amazon <mishoxuco2001@gmail.com>
2024-08-12 23:57:31 +0400

    responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
-import { useContext, useState } from "react";
+import quantoriLogo from "../../images/quantoriLogo.png";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import "./Home.css";
 export default function Home() {
-  const { login } = useContext(AuthContext);
+  const { login, setUser } = useContext(AuthContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -23,14 +24,22 @@ export default function Home() {
       });
 
       login(response.data.token, response.data.refreshToken);
-      navigate("/");
+
+      const data = await axios.get("https://dummyjson.com/auth/me", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${response.data.token}`,
+        },
+      });
+      setUser(data.data);
+      navigate("/about");
     } catch (error) {
       console.error("Login failed:", error);
     }
   };
-
   return (
-    <div className="login">
+    <div className="home">
+      <img src={quantoriLogo} alt="Quantori Logo" />
       <div className="loginInputs">
         <h2>Login</h2>
         <input
