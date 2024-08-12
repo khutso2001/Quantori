fbd6dcc0894c408487be8d3d509d396ec321777c
Amazon <mishoxuco2001@gmail.com>
2024-08-12 20:34:28 +0400

    responsive

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,18 @@
+import React, { useState, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-
-import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Footer from "../components/Footer";
 import "./Layout.css";
 
 const Layout = () => {
   const { token, logout } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
 
   return (
     <div>
@@ -33,7 +37,39 @@ const Layout = () => {
           ) : (
             <Link to="/login">Login</Link>
           )}
+          <FontAwesomeIcon
+            icon={menuOpen ? faTimes : faBars}
+            className="burgerIcon"
+            onClick={toggleMenu}
+          />
         </div>
+      </div>
+
+      <div className={`burgerMenu ${menuOpen ? "open" : ""}`}>
+        <Link to="/" className="burgerLink" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link to="/about" className="burgerLink" onClick={toggleMenu}>
+          About
+        </Link>
+        <Link to="/contact" className="burgerLink" onClick={toggleMenu}>
+          Contact
+        </Link>
+        {token ? (
+          <button
+            className="burgerButton"
+            onClick={() => {
+              logout();
+              toggleMenu();
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/login" className="burgerLink" onClick={toggleMenu}>
+            Login
+          </Link>
+        )}
       </div>
 
       <Outlet />
